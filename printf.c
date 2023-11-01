19e3957896024cd95379cd303f3c9d979f72b530
SsuessV <127782189+SsuessV@users.noreply.github.com>
2023-11-01 17:32:57 +0100

    01.11

--- printf.c
+++ printf.c
@@ -6,22 +6,227 @@
 /*   By: syoun <syoun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 15:57:08 by syoun             #+#    #+#             */
-/*   Updated: 2023/10/26 16:02:20 by syoun            ###   ########.fr       */
+/*   Updated: 2023/11/01 17:32:18 by syoun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-int ft_printf(const char *f_specifier, ...)
+r_specifier (const char *format, 
+{
+	int i;
+	i = 0;
+	if (*format == %)
+	{
+		format++;
+		if (*format == 'c')
+			i += ft_putchar ((long int) args)
+		else if (*format == 's')
+			i += ft_putstr ((char *) args)
+		else if (*format == 'p')
+		else if (*format == 'd')
+		else if (*format == 'i')
+		else if (*format == 'u')
+		else if (*format == 'x')
+		else if (*format == 'X')
+		else if (*format == '%')
+
+}
+
+
+int ft_printf (const char *format, ...)
 {
 	va_list args;
+	char *str;
 	int	i;
 
 	i = 0;
-	va_start (args, f_specifier);
-	while (i == '%')
+	while (*str)
 	{
-		
+		if (*str == '%')
+		{
+			str++;
+			if (ft_str("cs"))
+
+		}
+
 	}
+	va_start (args, format);
+	while (i < )
+	{
+		int value = va_arg(args, const char);
+	}
+	va_end (args)
 }
 
+int	main(void)
+{
+	char *format;
+	char *string;
+	char character;
+	int num;
+	int result_ft;
+	int result_print;
+	// ft_printf(format, 1, 2, 3);
+
+	puts("=== Character: %c ===");
+	format = "%c\n";
+	character = 'A';
+	result_ft = ft_printf(format, character);
+	result_print = printf(format, character);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	character = 'B';
+	result_ft = ft_printf(format, character);
+	result_print = printf(format, character);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+
+	puts("=== String: %s ===");
+	format = "%s\n";
+	string = NULL;
+	result_ft = ft_printf(format, string);
+	result_print = printf(format, string);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	string = "Hello I am a String";
+	result_ft = ft_printf(format, string);
+	result_print = printf(format, string);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+
+	puts("\n=== Integer: %d ===");
+	format = "%d\n";
+	num = 0;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = 42;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = -42;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = 2147483647;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = -2147483648;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+
+	puts("\n=== Hexa lower-Cased: %x ===");
+	format = "%x\n";
+	num = 0;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = 42;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = -42;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = 2147483647;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = -2147483648;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+
+	puts("\n=== Hexa UPPER-CASED: %X ===");
+	format = "%X\n";
+	num = 0;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = 42;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = -42;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = 2147483647;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = -2147483648;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+
+	puts("\n=== Multiple-Options (%d%s%X) ===");
+	format = "%d%s%X\n";
+	string = ": ";
+	num = 0;
+	result_ft = ft_printf(format, num, string, num);
+	result_print = printf(format, num, string, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = 42;
+	result_ft = ft_printf(format, num, string, num);
+	result_print = printf(format, num, string, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = -42;
+	result_ft = ft_printf(format, num, string, num);
+	result_print = printf(format, num, string, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = 2147483647;
+	result_ft = ft_printf(format, num, string, num);
+	result_print = printf(format, num, string, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	num = -2147483648;
+	result_ft = ft_printf(format, num, string, num);
+	result_print = printf(format, num, string, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+
+	puts("\n=== Percent: %% ===");
+	format = "%%\n";
+	result_ft = ft_printf(format);
+	result_print = printf("%%\n");
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+
+	puts("\n=== Pointer: %p ===");
+	format = "%p\n";
+	string = NULL;
+	result_ft = ft_printf(format, string);
+	result_print = printf(format, string);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	string = "Im a pointer";
+	result_ft = ft_printf(format, string);
+	result_print = printf(format, string);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	puts("\n=== Not supported Option and normal text: %y ===");
+	format = "Im not supported %y\n";
+	result_ft = ft_printf(format, string);
+	result_print = printf(format, string);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	puts("\n=== Custom Tests ===");
+	format = "%d%d\n";
+	num = 42;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	format = NULL;
+	num = 42;
+	result_ft = ft_printf(format, num);
+	result_print = printf(format, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+	format = "%c %s %p %d %i %u %x %X %%\n";
+	num = 42;
+	char c = '4';
+	string = "printf";
+	result_ft = ft_printf(format, c, string, string, num, num, num, num, num);
+	result_print = printf(format, c, string, string, num, num, num, num, num);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+
+	puts("\n === Check max Pointer ===");
+	format = "%p\n";
+	result_ft = ft_printf(format, 18446744073709551615ULL);
+	result_print = printf(format, 18446744073709551615ULL);
+	printf("Return: ft_printf=%d printf=%d\n\n", result_ft, result_print);
+}
